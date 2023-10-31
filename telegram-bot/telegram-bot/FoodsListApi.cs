254a0f41a852c25bbe91e7f5ad917be927aec876
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2023-10-31 15:53:54 +0330

    add api to get list of foods

---
+++ telegram-bot/telegram-bot/FoodsListApi.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+namespace telegram_bot
+{
+    public class FoodsListApi
+    {
+        public string Food { get; set; }
+        string API_KEY = "9f9d2d880dd34413bfb2855fbbaec75c";
+
+        public FoodsListApi(string food)
+        {
+            this.Food = food;
+        }
+
+        public async Task<List<Food>> GetFoodsList()
+        {
+
+            HttpClient client = new HttpClient();
+            string endpint = $"https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}&query={Food}";
+            HttpResponseMessage response = await client.GetAsync(endpint);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+
+                FoodsList data = JsonConvert.DeserializeObject<FoodsList>(apiResponse);
+
+                return data.Results;
+            }
+            else
+            {
+                throw new Exception("Failed to get list of foods.");
+            }
+
+        }
+
+    }
+}
