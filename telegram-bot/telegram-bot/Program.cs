3b74d764e732f0e8c4eae9efb960079656c830f3
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2023-10-31 16:10:31 +0330

    commented full name and token

--- telegram-bot/telegram-bot/Program.cs
+++ telegram-bot/telegram-bot/Program.cs
@@ -1,5 +1,8 @@
 ﻿using telegram_bot;
 
+// My FullName is Mohammad Hossein Jafari
+
+// my token: 6623617510:AAG3F3ZwmnXO6st6MuMdGTX4E2YU-guKK8o
 
 // replace {TOKEN} with the token that you get from BotFather
 var bot = new TelegramBot("{TOKEN}");
