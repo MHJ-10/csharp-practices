e0016f1c420740ed8fbae90d8b75d2e7edfc20d2
Mohammad Hossein Jafari <mhj10mhj10jafari@gmail.com>
2023-10-31 15:55:51 +0330

    add food instruction

---
+++ telegram-bot/telegram-bot/FoodInstruction.cs
@@ -0,0 +1,16 @@
+﻿namespace telegram_bot
+{
+    public class FoodInstruction
+    {
+        public List<Step>? AnalyzedInstructions { get; set; }
+    }
+    public class Step
+    {
+        public List<StepItem>? Steps { get; set; }
+    }
+    public class StepItem
+    {
+        public int? Number { get; set; }
+        public string? Step { get; set; }
+    }
+}
